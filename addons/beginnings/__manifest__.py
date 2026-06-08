a4de6b8deb435684fc8981f35cabeef9606f6d04
Nigel van Keulen <nigel2392@gmail.com>
2026-06-08 13:43:39 +0200

    Start app odoo beginnings

---
+++ addons/beginnings/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Odoo Beginnings",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Nigel2392",
+    'category': 'learning',
+    'description': """
+    Learning the basics of the Odoo platform
+    """,
+    # data files always loaded at installation
+    # 'data': [
+    #     'views/beginnings.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
