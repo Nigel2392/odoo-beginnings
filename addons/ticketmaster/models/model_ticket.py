455267a7d294202b4b7cc79d39873d9b370c638e
Nigel van Keulen <nigel2392@gmail.com>
2026-06-09 22:30:33 +0200

    add translations to models

--- addons/ticketmaster/models/model_ticket.py
+++ addons/ticketmaster/models/model_ticket.py
@@ -1,11 +1,10 @@
-from odoo import api, fields, models
-
+from odoo import _, api, fields, models
 
 class Ticket(models.Model):
     _name = "ticketmaster.ticket"
     _description = """Support Ticket"""
 
-    title = fields.Char(string="Title", required=True)
-    description = fields.Text(string="Description", required=True)
-    date = fields.Date(string="Date Created", default=fields.Date.today())
+    title = fields.Char(string=_("Title"), required=True)
+    description = fields.Text(string=_("Description"), required=True)
+    date = fields.Date(string=_("Date Created"), default=fields.Date.today())
 
--- addons/ticketmaster/models/model_ticket_reply.py
+++ addons/ticketmaster/models/model_ticket_reply.py
@@ -1,11 +1,11 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class TicketReply(models.Model):
     _name = "ticketmaster.reply"
     _description = """Reply to support ticket"""
 
-    master_id = fields.Many2one("ticketmaster.ticket", string="Ticket", required=True)
-    user = fields.Many2one("res.users", string="User", required=True)
-    date = fields.Date(string="Date Created", default=fields.Date.today())
-    description = fields.Text(string="Description", required=True)
+    master_id = fields.Many2one("ticketmaster.ticket", string=_("Ticket"), required=True)
+    user = fields.Many2one("res.users", string=_("User"), required=True)
+    date = fields.Date(string=_("Date Created"), default=fields.Date.today())
+    description = fields.Text(string=_("Description"), required=True)
