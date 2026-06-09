dab70d7245ae1cd3dddae5d0394d10edc853309d
Nigel van Keulen <nigel2392@gmail.com>
2026-06-09 22:30:25 +0200

    add status model

--- addons/ticketmaster/models/model_ticket_status.py
+++ addons/ticketmaster/models/model_ticket_status.py
@@ -1,6 +1,9 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class TicketStatus(models.Model):
     _name = "ticketmaster.status"
     _description = """Status for support ticket"""
+
+    name = fields.Char(string=_("Status"), required=True)
+    color = fields.Char(string=_("HTML Color"), default="#ffffff")
