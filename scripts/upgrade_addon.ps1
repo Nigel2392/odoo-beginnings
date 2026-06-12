2e83aa6eb4d8c25a47ddda5b4951268647b0cb66
Nigel van Keulen <nigel2392@gmail.com>
2026-06-12 14:49:42 +0200

    upgrade_ticketmaster.ps1 -> upgrade_addon.ps1
    allow script params

---
+++ scripts/upgrade_addon.ps1
@@ -0,0 +1,20 @@
+param(
+    [string]$Mod="ticketmaster",
+    [string]$Database="odoo-beginnings",
+    [string]$PasswordFile="odoo_pg_pass"
+)
+
+# Retrieves the postgress password from the root project directory.
+# Updates the ticketmaster(or other) module.
+
+$parentDir = Split-Path -Parent $PSScriptRoot
+$passwordPath = Join-Path $parentDir $PasswordFile
+$password = (Get-Content -Path $passwordPath -Raw).Trim()
+
+docker exec -it odoo odoo `
+  -u $Mod `
+  -d $Database `
+  --db_host=db `
+  --db_user=odoo `
+  --db_password=$password `
+  --stop-after-init
